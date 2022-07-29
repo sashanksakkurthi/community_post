1b5d5832d0bacf2f8c39243ffda970f1da87cda4
Sakkurthi Sashank <sakkurthisashank@gmail.com>
2022-07-29 22:16:46 +0530

    changed to tailwind

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,29 +1,7 @@
 import type { NextPage } from "next";
-import Navbar from "../components/Navbar";
-import { Progress, Stack } from "@chakra-ui/react";
-import PostInput from "../components/PostInput";
-import PostCard from "../components/PostCard";
-import { useState } from "react";
+
 const Home: NextPage = () => {
-  const [logging, setLogging] = useState(false);
-  setTimeout(() => {
-    setLogging(true);
-  }, 500);
-  return (
-    <>
-      {logging ? (
-        <>
-          <Navbar />
-          <Stack gap={2}>
-            <PostInput />
-            <PostCard />
-          </Stack>
-        </>
-      ) : (
-        <Progress size="xs" isIndeterminate />
-      )}
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
