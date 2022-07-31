5e0b25524533de1aba47c4a3583961d530c2a3c5
Sakkurthi Sashank <sakkurthisashank@gmail.com>
2022-07-31 17:37:34 +0530

    dotenv deleted

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import express, { Request, Response, Application } from "express";
 import { Server } from "http";
 import router from "./router";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app: Application = express();
 
