ff612965916959b03c5e21627f301d5cb4855c79
Sakkurthi Sashank <sakkurthisashank@gmail.com>
2022-07-31 17:06:43 +0530

    check

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Request, Response, Application } from "express";
 import { Server } from "http";
 import router from "./router";
 import cors from "cors";
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("App is Working");
+});
 
 const PORT = process.env.PORT || 8080;
 const server: Server = app.listen(PORT, () => {
